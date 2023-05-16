37ab2ca502f4b611b697a45b1a46b2325cc2c4b4
dneog <debashisneog23@gmail.com>
2023-05-16 19:52:03 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import MovieList from './MovieList';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading]= useState(false);
+  const [error, setError]= useState(null);
+  const [retry, setRetry]= useState(0);
+  const [dataRetry, setDataRetry]= useState(true)
+  const [userData, setUserData]= useState(null)
+ 
+  useEffect(()=> {
+    if(userData=== null){
 
-  async function handleMovies(){
-    setIsLoading(true)
-    const response = await fetch('https://swapi.py4e.com/api/films/');
-    const data = await response.json();
+    
+    handleMovies()
+    }
+  }, [retry])
 
-    const movieList = data.results.map((movieData)=> {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
+    async function handleMovies(){
+      setIsLoading(true);
+      setError(null);
+      try{
+        const response = await fetch('https://swapi.py4e.com/api/film/');
+        if(!response.ok){
+          throw new Error('Something went wrong ...Retrying')
+        }
+        const data = await response.json();
+        setUserData(data)
+       
+    
+        const movieList = data.results.map((movieData)=> {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          }
+        })
+        setMovies(movieList)
+        
+  
+      }catch(error){
+        if(dataRetry){
+
+        
+        setError(error.message)
+        retryData();
+        }else{
+         
+        }
       }
-    })
-    setMovies(movieList)
-    setIsLoading(false)
+      setIsLoading(false)
+      
+    }
+
+  function handleRetry(){
+   setDataRetry(false)
   }
+
+  const retryData=()=> {
+    setTimeout(()=> {
+      setRetry(retry+1)
+    }, 5000)
+  }
+
   
   
   return (
@@ -35,6 +78,8 @@ function App() {
     {!isLoading && movies.map((movie)=> (
       <MovieList movies= {movie} />
     ))}
+    {!isLoading && error && <p>{error}</p> }
+    {retry && <button onClick={handleRetry}>Cancel Retrying</button>}
       
     </section>
 
